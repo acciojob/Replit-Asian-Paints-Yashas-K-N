95e1e08e7f19404b6a2f00a4ebd08701172a1e13
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2025-03-17 21:28:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const gridContainer= document.querySelector(".grid")
+ const form =document.querySelector("#form")
+let previousSelectedElement = null;
+
+
+ form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let cellId = form.inputId.value;
+    let selectedColor = form.color.value;
+     
+    let cell = document.getElementById(cellId);
+      cell.style.backgroundColor = selectedColor;
+      if(previousSelectedElement){
+         previousSelectedElement.style.backgroundColor = "white";
+            }
+    
+      previousSelectedElement = cell;
+      
+ });
+ for (let i=1; i<=9; i++){
+    const gridItem = document.createElement("div");
+     gridItem.id = i; 
+     gridItem.innerText=i
+     gridItem.className="grid-Item";
+     gridContainer.appendChild(gridItem);
+
+ }
+
+//  document.addEventListener("contextmenu",(event)=>{
+//     event.preventDefault();
+//     console.log("inside submit button")
+//  });
